ccfe3f73a9f733b9b364a62ef7adc07fff942405
Vince Larkin <carrarn@gmail.com>
2018-12-29 21:27:03 +0100

    First Enemy and audio slots
    Player rotate ipv straved
    Enemy is statisch en soms rood soms groen
    Audio slots op character en enemy

--- Assets/Scripts/Character.cs
+++ Assets/Scripts/Character.cs
@@ -10,6 +10,12 @@ public class Character : MonoBehaviour
     public float jumpForce;
 	private Rigidbody myRigidbody;	
     private Vector3 jump;
+    private Vector3 eulerAngleVelocity;
+
+     private AudioSource myAudioSource;
+    public AudioClip jumpClip;
+
+    private bool isDead;
 
     /// <summary>
     /// Start is called on the frame when a script is enabled just before
@@ -17,6 +23,9 @@ public class Character : MonoBehaviour
     /// </summary>
     void Start()
     {
+        this.isDead = false;
+        this.myAudioSource = this.GetComponent<AudioSource>();
+        this.eulerAngleVelocity = new Vector3(0,100,0);
         this.myRigidbody = this.GetComponent<Rigidbody>();
         this.jump = new Vector3(0.0f, 2.0f, 0.0f);
         this.speed = 10f;	
@@ -27,12 +36,26 @@ public class Character : MonoBehaviour
     /// This function is called every fixed framerate frame, if the MonoBehaviour is enabled.
     /// </summary>
     void FixedUpdate() {						
+        // if(Input.GetKey(KeyCode.RightArrow)) {
+        //     this.myRigidbody.MovePosition(transform.position + transform.right * Time.deltaTime * this.speed);
+        // }
+
+        // if(Input.GetKey(KeyCode.LeftArrow)) {
+        //     this.myRigidbody.MovePosition(transform.position + -transform.right * Time.deltaTime * this.speed);
+        // }
+
+        if(this.isDead) {
+            return;
+        }
+
         if(Input.GetKey(KeyCode.RightArrow)) {
-            this.myRigidbody.MovePosition(transform.position + transform.right * Time.deltaTime * this.speed);
+            Quaternion deltaRotation = Quaternion.Euler(this.eulerAngleVelocity * Time.deltaTime);
+            this.myRigidbody.MoveRotation(this.myRigidbody.rotation * deltaRotation);
         }
 
         if(Input.GetKey(KeyCode.LeftArrow)) {
-            this.myRigidbody.MovePosition(transform.position + -transform.right * Time.deltaTime * this.speed);
+            Quaternion deltaRotation = Quaternion.Euler(-this.eulerAngleVelocity * Time.deltaTime);
+            this.myRigidbody.MoveRotation(this.myRigidbody.rotation * deltaRotation);
         }
 
         if(Input.GetKey(KeyCode.UpArrow)) {
@@ -44,8 +67,14 @@ public class Character : MonoBehaviour
         }
 
         //JUMPEHHH
-        if(Input.GetKey(KeyCode.Space)) {			        					 
+        if(Input.GetKey(KeyCode.Space)) {		
+            this.myAudioSource.Play();	        					 
             this.myRigidbody.AddForce(jump * jumpForce, ForceMode.Force);                
         }		
 	}
+
+    public void killYourSelf() {
+        this.isDead = true;
+        Destroy(this.playerGraphics);
+    }
 }
---
+++ Assets/Scripts/Enemy.cs
@@ -0,0 +1,80 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Enemy : MonoBehaviour
+{
+
+    public GameObject enemyGraphics;
+    public float speed;
+    private AudioSource myAudioSource;
+    public AudioClip bamClip;
+    public AudioClip explosionClip;
+
+    private bool isDangerous = true;
+    public float timeTillDangerous;
+    private float nextTime;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        this.myAudioSource = GetComponent<AudioSource>();
+        this.speed = 10f;
+        this.timeTillDangerous = 2f;
+        this.nextTime = Time.time + this.timeTillDangerous;
+
+        this.setColor(Color.green);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Time.time >= nextTime)
+        {            
+            this.isDangerous = !this.isDangerous;
+
+            if (this.isDangerous)
+            {
+                this.setColor(Color.red);
+            }
+            else
+            {
+                this.setColor(Color.green);
+            }
+            this.nextTime = Time.time + this.timeTillDangerous;
+        }
+    }
+
+    /// <summary>
+    /// OnCollisionEnter is called when this collider/rigidbody has begun
+    /// touching another rigidbody/collider.
+    /// </summary>
+    /// <param name="other">The Collision data associated with this collision.</param>
+    void OnCollisionEnter(Collision other)
+    {
+        if (other.transform.tag == "Player")
+        {
+            if (isDangerous)
+            {
+                // play dead sound and - destroy player cube     
+                this.myAudioSource.clip = this.explosionClip;
+                this.myAudioSource.Play();
+
+                var character = other.gameObject.GetComponent<Character>();
+                character.killYourSelf();
+            }
+            else
+            {
+                // play bam sound - and freeze enemy movement?
+                this.myAudioSource.clip = this.bamClip;
+                this.myAudioSource.Play();
+            }
+        }
+    }
+
+    private void setColor(Color color)
+    {
+        this.enemyGraphics.GetComponent<MeshRenderer>().material.color = color;
+    }
+
+}
